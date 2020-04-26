9817f60ee2678cc1483937068e0cdcfd6c85671e
Mouhamedou <44713284+mouhamedu@users.noreply.github.com>
2020-04-26 03:15:30 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 new WOW().init();
 
 // the main variables
-// api key
-let api = "07882d3bfddaeedc0df6afc56c3fac7d";
 // api key for omdbapi
 let omdbapi = "b77b8f3e";
+// api key for the movie db
+let api = "07882d3bfddaeedc0df6afc56c3fac7d";
 // search input
 let search = document.querySelector('input[type="text"]');
 // the main images url
@@ -30,7 +30,7 @@ search.addEventListener('keydown', (event) => {
 		document.querySelector('.single-row').innerHTML = "";
 		
 		// the main url
-		let url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=${search.value}`;
+		let url = `https://api.themoviedb.org/3/search/movie?api_key=${api}&query=${search.value}&language=en-US&include_adult=false`;
 
 		// fetch the movie information   
 		fetch(url)
@@ -39,20 +39,27 @@ search.addEventListener('keydown', (event) => {
 			return data.json();
 
 		}).then((result) => {
+			console.log(result.results[4]);
 			// working with final result
 			// check if the input value return any result
 			if (result.results != 0) {
 
+				// clear the text in the title
 				title.innerText = "Searching Result";
 				title.style = "padding: 50px 0;";
 				movies_container.innerHTML = result.results.map((item) => {
-					
-					// if the movie have poster => image 
+					// if the movie have poster => image add it to the movies container
 					if (item.poster_path) {
+
+						// remove any single or double qoutes in the discription and the title of the movie
+						let regEx = /['"]+/gm;
+						let disc_without_qoutes = item.overview.replace(regEx, "");
+						let title_without_qoutes = item.title.replace(regEx, "");
+						// return the details about the movie
 						return `
 							<div class="col">
 								<div class="zoom">
-									<img src="${imageUrl + item.poster_path}" alt="movie-image" title="${item.title}" onclick="selectedItem('${item.id}')" class="wow zoomIn">
+									<img src="${imageUrl + item.poster_path}" alt="movie-image" title="${item.title}" onclick='selectedItem("${imageUrl + item.poster_path}", "${title_without_qoutes}", "${item.release_date}", "${disc_without_qoutes}")' class="wow zoomIn">
 								</div>
 							</div>
 							`;
@@ -67,11 +74,11 @@ search.addEventListener('keydown', (event) => {
 				movies_container.innerHTML = "";
 			}
 
-			// wait 2 second to fetch all the resutl then locate the page to the final result
+			// wait 2 second to fetch all the resutl then redirect to the final result
 			setTimeout(() => {
 				// click the link with click method
 				document.querySelector('a').click();
-			}, 1000);
+			}, 2000);
 
 		});
 
@@ -80,56 +87,25 @@ search.addEventListener('keydown', (event) => {
 });
 
 // this function to set the item when the user click on the movie thumbnail
-function selectedItem(id) {
+function selectedItem(thumbnail, title, release, discription) {
 
-	// fetch the full data of the movie using it's id to get the imdb api id 
-	// so with imdb api id we can get a better details about the movie
-	
-	fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${api}`)
-	.then((data) => {
+	// to show the modal 
+	modal.style.display = 'block';
+	// to push the data inside the single row
+	document.querySelector('.single-row').innerHTML = `
 
-		return data.json();
-
-	}).then((result) => {
-
-		// store the movie imdb id 
-		let theMovieId = result.imdb_id;
-
-		// fetching the movie details with omdbapi
-		fetch(`http://www.omdbapi.com/?i=${theMovieId}&apikey=${omdbapi}`)
-		.then((data) => {
-			return data.json();
-		}).then((result) => {
-			
-			// show modal container 
-			modal.style = 'display: block;';
-			
-			// add the final content to single-row 
-			document.querySelector('.single-row').innerHTML = `
-
-				<div class="col">
-					<div class="zoom">
-						<img src="${result.Poster}" alt="movie-image">
-					</div>
-				</div>
-				<div class="col">
-					<h1>${result.Title}</h1>
-					<p>Release: ${result.Released}</p>
-					<p>Runtime: ${result.Runtime}</p>
-					<p>type: ${result.Genre}</p>
-					<p>Language: ${result.Language}</p>
-					<p>Actors: ${result.Actors}</p>
-					<p>Director: ${result.Director}</p>
-					<p>Writer: ${result.Writer}</p>
-					<p>Discriptoin: ${result.Plot}</p>
-				</div>
+		<div class="col">
+			<div class="zoom">
+				<img src="${thumbnail}" alt="movie-image">
+			</div>
+		</div>
+		<div class="col">
+			<h1>${title}</h1>
+			<p>Release: ${release}</p>
+			<p>Discription: ${discription}</>
+		</div>
 				
-			`
-
-		});
-
-	});
-
+	`;
 
 }
 
